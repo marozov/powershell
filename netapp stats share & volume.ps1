ca28bfa73bdff6183980b0f87b1e4c9bcc7b2ed6
Pavel Peleshenko <pavlik.marozov@gmail.com>
2024-02-28 16:14:06 +0500

    Update netapp stats share & volume.ps1
    Улучшайзер 2.0

--- netapp stats share & volume.ps1
+++ netapp stats share & volume.ps1
@@ -1,10 +1,10 @@
 # Импортируем модуль NetApp PowerShell SDK
 Import-Module DataONTAP
 # Установите имя вашей шары
-$shareName = "share_name" 
+$shareName = "Baza" 
 # Устанавливаем учетные данные для доступа к контроллерам
-$Username = "user_admin"
-$Password = "pas_xxxxx"
+$Username = "XXXXXXX"
+$Password = "XXXXXXX"
 $Credential = New-Object System.Management.Automation.PSCredential($Username, (ConvertTo-SecureString $Password -AsPlainText -Force))
 
 # IP-адреса контроллеров
@@ -22,16 +22,22 @@ function Find-VolumeOnController {
         # Подключаемся к контроллеру
         Connect-NcController -Name $Controller -Credential $Credential -ErrorAction Stop
 
-        # Получаем список 
+        # Получаем список шар из qtreeList
         $qtreeList = Get-NcQtree
+        #
         $qtree = $qtreeList | Where-Object { $_.Qtree -eq $shareName } -ErrorAction Stop
-         #$qtree.NcController
+        #
         $qtreeUsed = Get-NcQuotaReport -QuotaType tree -Qtree $qtree.Qtree | Select-Object DiskUsed
+        #
         $qtreeSize = Get-NcQuotaReport -QuotaType tree -Qtree $qtree.Qtree | Select-Object DiskLimit
+        #
         $volumeqtree = $qtree.Volume
+        #
         $volumeName = Get-NcVol | Where-Object { $_.Name -eq $volumeqtree }
+        $qtreeName = $qtree.Qtree
+        #
 
-        # Проверяем, есть ли шары и форматируем вывод
+        # Проверяем, есть ли шары
         if ($qtree) {
             Write-Output "Шары найдены на контроллере $Controller :"
             $volumeSpace = ($volumeName.TotalSize / 1024MB).ToString("F0")
@@ -47,22 +53,22 @@ function Find-VolumeOnController {
             $diskAvailablePercent = if ($qtreeSizeInGb -eq 0) { 0 } else { (($qtreeUsedInGb / $qtreeSizeInGb) * 100).ToString("F0")
             }
             }
-            Write-Output "_______________________"
-            Write-Output "Ресурс: $shareName "
+            Write-Output "________________________________"
+            Write-Output "Ресурс: $qtreeName "
             #
             if ($qtreeSize.DiskLimit -eq "-") {
     Write-Output "Размер ресурса: $qtreeSizeInGb "
 } else {
     Write-Output "Размер ресурса: $qtreeSizeInGb GiB"
 }
-            #
-            Write-Output "Использовано на ресурсе: $qtreeUsedInGb GiB"
-            Write-Output "Использовано на ресурсе: $diskAvailablePercent %"
+            #Write-Output "Размер ресурса: $qtreeSizeInGb "
+            Write-Output "Использовано на ресурсе: $qtreeUsedInGb GiB $diskAvailablePercent %"
+            #Write-Output "Использовано на ресурсе: $diskAvailablePercent %"
             #Write-Output "Volume Name: $($volumeName)"
             Write-Output "Размер тома: $($volumeSpace) GiB"
             #Write-Output "Volume Used: $($volumeName.Used) %"
             Write-Output "Доступно на томе: $($volumeAvailable) GiB"
-            Write-Output "_______________________"
+            Write-Output "________________________________"
         } else {
             Write-Output "Шары не найдены на контроллере $Controller ."
         }
